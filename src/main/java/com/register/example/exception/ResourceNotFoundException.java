b76e56603a424e2fb5f72e6db5a2116c8e6cc8c6
Admin-Xevyte <admin@xevyte.com>
2025-09-01 17:15:27 +0530

    Delete src directory

--- src/main/java/com/register/example/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.register.example.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/register/example/repository/ClaimHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.register.example.repository;
-
-import com.register.example.entity.ClaimHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClaimHistoryRepository extends JpaRepository<ClaimHistory, Long> {
-}
