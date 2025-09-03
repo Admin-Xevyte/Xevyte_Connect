f7ef95127e93de64ea6351955ac7420e26ab630b
Admin-Xevyte <admin@xevyte.com>
2025-09-03 13:23:17 +0530

    Update ClaimsPage.js

--- employee-login-portal/src/pages/ClaimsPage.js
+++ employee-login-portal/src/pages/ClaimsPage.js
@@ -26,7 +26,7 @@ function ClaimsPage() {
 
   useEffect(() => {
     if (employeeId) {
-      fetch(`http://13.234.30.186:8080/claims/summary/${employeeId}`)
+      fetch(`http://3.7.139.212:8080/claims/summary/${employeeId}`)
         .then(res => res.json())
         .then(data => {
           setSummary({
@@ -42,7 +42,7 @@ function ClaimsPage() {
 
   useEffect(() => {
     if (employeeId) {
-      fetch(`http://13.234.30.186:8080/profile/${employeeId}`)
+      fetch(`http://3.7.139.212:8080/profile/${employeeId}`)
         .then(res => res.json())
         .then(data => {
           if (data.profilePic) {
@@ -94,7 +94,7 @@ function ClaimsPage() {
     formData.append("profilePic", file);
 
     try {
-      const res = await fetch(`http://13.234.30.186:8080/profile/update/${employeeId}`, {
+      const res = await fetch(`http://3.7.139.212:8080/profile/update/${employeeId}`, {
         method: "PUT",
         body: formData,
       });
