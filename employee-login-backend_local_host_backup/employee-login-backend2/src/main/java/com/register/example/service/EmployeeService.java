871e55ace42f33ae75768f99374d745c3e2539c4
Admin-Xevyte <admin@xevyte.com>
2025-09-14 01:01:13 +0530

    Create EmployeeService.java

---
+++ employee-login-backend_local_host_backup/employee-login-backend2/src/main/java/com/register/example/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.register.example.service;
+
+import com.register.example.entity.Employee;
+import com.register.example.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
