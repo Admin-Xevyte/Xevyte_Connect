5cfbc1f2ba5667c887c55aad8bcf25cf4cbf4041
Admin-Xevyte <admin@xevyte.com>
2025-09-14 10:48:08 +0530

    Update ManagerDashBoard.js

--- employee-login-portal_backup_local/employee-login-portal/src/pages/ManagerDashBoard.js
+++ employee-login-portal_backup_local/employee-login-portal/src/pages/ManagerDashBoard.js
@@ -7,11 +7,10 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
-// Correct way for mjs worker
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url
-).toString();
+
+pdfjs.GlobalWorkerOptions.workerSrc =
+  `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
 
 function ManagerDashBoard() {
   const [claims, setClaims] = useState([]);
