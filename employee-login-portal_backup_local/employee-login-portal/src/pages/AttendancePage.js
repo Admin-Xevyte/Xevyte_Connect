d65e81a2730719d58d83220ff9025c9564a0bdaf
Admin-Xevyte <admin@xevyte.com>
2025-09-15 10:30:22 +0530

    Update AttendancePage.js

--- employee-login-portal_backup_local/employee-login-portal/src/pages/AttendancePage.js
+++ employee-login-portal_backup_local/employee-login-portal/src/pages/AttendancePage.js
@@ -65,7 +65,7 @@ const cardStyle = {
 
 useEffect(() => {
   if (employeeId) {
-    fetch(`/access/assigned-ids/${employeeId}`)
+    fetch(`http://3.7.139.212:8080/access/assigned-ids/${employeeId}`)
       .then(res => res.json())
       .then(data => {
         setRoles(data);
