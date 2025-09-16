3b483ac37de5863b5df992ee8e50166738dd537e
Admin-Xevyte <admin@xevyte.com>
2025-09-16 22:40:19 +0530

    Update Myteam.js

--- employee-login-portal_backup_local/employee-login-portal/src/pages/Myteam.js
+++ employee-login-portal_backup_local/employee-login-portal/src/pages/Myteam.js
@@ -70,7 +70,7 @@ const [roles, setRoles] = useState({
 
 useEffect(() => {
   if (employeeId) {
-    fetch(`/access/assigned-ids/${employeeId}`)
+    fetch(`http://3.7.139.212:8080/access/assigned-ids/${employeeId}`)
       .then(res => res.json())
       .then(data => {
         setRoles({
@@ -86,7 +86,7 @@ useEffect(() => {
 
   useEffect(() => {
     if (employeeId) {
-      fetch(`/profile/${employeeId}`)
+      fetch(`http://3.7.139.212:8080/profile/${employeeId}`)
         .then(res => res.json())
         .then(data => {
           if (data.profilePic) {
@@ -137,7 +137,7 @@ useEffect(() => {
     formData.append("profilePic", file);
 
     try {
-      const res = await fetch(`/profile/update/${employeeId}`, {
+      const res = await fetch(`http://3.7.139.212:8080/profile/update/${employeeId}`, {
         method: "PUT",
         body: formData,
       });
