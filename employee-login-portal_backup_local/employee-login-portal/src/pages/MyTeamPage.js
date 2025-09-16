cb0d9812b248d53c83309ce15b15009a2eee9a21
Admin-Xevyte <admin@xevyte.com>
2025-09-16 22:42:45 +0530

    Update MyTeamPage.js

--- employee-login-portal_backup_local/employee-login-portal/src/pages/MyTeamPage.js
+++ employee-login-portal_backup_local/employee-login-portal/src/pages/MyTeamPage.js
@@ -52,7 +52,7 @@ const MyTeamPage = () => {
 
   useEffect(() => {
     if (employeeId) {
-      fetch(`/profile/${employeeId}`)
+      fetch(`http://3.7.139.212:8080/profile/${employeeId}`)
         .then(res => res.json())
         .then(data => {
           if (data.profilePic) {
@@ -103,7 +103,7 @@ const MyTeamPage = () => {
     formData.append("profilePic", file);
 
     try {
-      const res = await fetch(`/profile/update/${employeeId}`, {
+      const res = await fetch(`http://3.7.139.212:8080/profile/update/${employeeId}`, {
         method: "PUT",
         body: formData,
       });
