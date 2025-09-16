f24c9aba89bc90f31a3f1fc69ad27aab40c744c7
Admin-Xevyte <admin@xevyte.com>
2025-09-16 22:34:49 +0530

    Update Performance.js

--- employee-login-portal_backup_local/employee-login-portal/src/pages/Performance.js
+++ employee-login-portal_backup_local/employee-login-portal/src/pages/Performance.js
@@ -24,7 +24,7 @@ function Performance() {
  
   useEffect(() => {
     if (employeeId) {
-      fetch(`/profile/${employeeId}`)
+      fetch(`http://3.7.139.212:8080/profile/${employeeId}`)
         .then(res => res.json())
         .then(data => {
           if (data.profilePic) {
@@ -75,7 +75,7 @@ function Performance() {
     formData.append("profilePic", file);
  
     try {
-      const res = await fetch(`/profile/update/${employeeId}`, {
+      const res = await fetch(`http://3.7.139.212:8080/profile/update/${employeeId}`, {
         method: "PUT",
         body: formData,
       });
