8063c7865a16390d561746b0f4d57713d8ca6af6
Admin-Xevyte <admin@xevyte.com>
2025-09-16 23:07:46 +0530

    Update PerformanceEmployeeRepository.java

--- employee-login-backend_local_host_backup/employee-login-backend2/src/main/java/com/register/example/repository/PerformanceEmployeeRepository.java
+++ employee-login-backend_local_host_backup/employee-login-backend2/src/main/java/com/register/example/repository/PerformanceEmployeeRepository.java
@@ -1,11 +1,12 @@
 package com.register.example.repository;
-
+ 
 import com.register.example.entity.PerformanceEmployee;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+ 
 import java.util.List;
-
+ 
 public interface PerformanceEmployeeRepository extends JpaRepository<PerformanceEmployee, String> {
-
+ 
     List<PerformanceEmployee> findByManagerId(String managerId);
 }
+ 
