bac38a7315ae1ec4ca13d5710cc5e8985aeea4e3
Admin-Xevyte <admin@xevyte.com>
2025-09-16 22:54:43 +0530

    Update Finalhrgoals.js

--- employee-login-portal_backup_local/employee-login-portal/src/pages/Finalhrgoals.js
+++ employee-login-portal_backup_local/employee-login-portal/src/pages/Finalhrgoals.js
@@ -43,7 +43,7 @@ function FinalHrGoalsWithLayout() {
   // Fetch updated profile info
   useEffect(() => {
     if (employeeIdFromStorage) {
-      fetch(`/profile/${employeeIdFromStorage}`)
+      fetch(`http://3.7.139.212:8080/profile/${employeeIdFromStorage}`)
         .then(res => res.json())
         .then(data => {
           if (data.profilePic) {
@@ -94,7 +94,7 @@ function FinalHrGoalsWithLayout() {
     formData.append("profilePic", file);
 
     try {
-      const res = await fetch(`/profile/update/${employeeIdFromStorage}`, {
+      const res = await fetch(`http://3.7.139.212:8080/profile/update/${employeeIdFromStorage}`, {
         method: "PUT",
         body: formData,
       });
@@ -152,7 +152,7 @@ function FinalHrGoalsWithLayout() {
       }
       const token = `Bearer ${rawToken}`;
 
-      const response = await fetch(`/api/goals/employee/${employeeId}`, {
+      const response = await fetch(`http://3.7.139.212:8080/api/goals/employee/${employeeId}`, {
         method: 'GET',
         headers: { Authorization: token, 'Content-Type': 'application/json' },
       });
@@ -180,7 +180,7 @@ function FinalHrGoalsWithLayout() {
         rawToken = rawToken.slice(1, -1);
       }
       const token = `Bearer ${rawToken}`;
-      const response = await fetch(`/api/goals/${goalId}/comments`, {
+      const response = await fetch(`http://3.7.139.212:8080/api/goals/${goalId}/comments`, {
         headers: { Authorization: token, 'Content-Type': 'application/json' }
       });
       if (!response.ok) {
