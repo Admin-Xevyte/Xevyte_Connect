a717f2e25502e890ee86fa93171909483c1773a5
Admin-Xevyte <admin@xevyte.com>
2025-09-17 15:14:06 +0530

    Update FinanceDashboard.js

--- employee-login-portal_backup_local/employee-login-portal/src/pages/FinanceDashboard.js
+++ employee-login-portal_backup_local/employee-login-portal/src/pages/FinanceDashboard.js
@@ -8,11 +8,7 @@ import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
 
-// ✅ Correct workerSrc for CRA + ESM worker:
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `./pdf.worker.min.js`;
 
 function FinanceDashboard() {
   const [claims, setClaims] = useState([]);
