add66cd99bbabc8112741cd4ba31834995c381e2
Admin-Xevyte <admin@xevyte.com>
2025-09-17 14:37:17 +0530

    Update FinanceDashboard.js

--- employee-login-portal_backup_local/employee-login-portal/src/pages/FinanceDashboard.js
+++ employee-login-portal_backup_local/employee-login-portal/src/pages/FinanceDashboard.js
@@ -7,7 +7,11 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = 'http://3.7.139.212/pdf.worker.min.js';
+// Correct way for mjs worker
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url
+).toString();
 
 
 function FinanceDashboard() {
